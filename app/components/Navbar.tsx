1d0be7a695b7b0c7554c314df04562c698da928c
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2024-03-06 02:16:50 +0330

    add profile menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { NavLinks } from "../constant";
 import AuthProviders from "./AuthProviders";
 import { getCurrentUser } from "../lib/session";
+import ProfileMenu from "./ProfileMenu";
 
 const Navbar = async () => {
   const session = await getCurrentUser();
@@ -25,9 +26,9 @@ const Navbar = async () => {
       <div className="flexCenter gap-4">
         {session?.user ? (
           <>
-            {/* <ProfileMenu session={session} /> */}
+            <ProfileMenu session={session} />
             <Link href="/create-project">
-              {session?.user.name}
+              Share work
               {/* <Button title='Share work' /> */}
             </Link>
           </>
--- app/components/ProfileMenu.tsx
+++ app/components/ProfileMenu.tsx
@@ -1,7 +1,74 @@
-import React from "react";
+"use client";
 
-const ProfileMenu = () => {
-  return <div>ProfileMenu</div>;
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { signOut } from "next-auth/react";
+import { SessionInterface } from "@/common.types";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const ProfileMenu = ({ session }: { session: SessionInterface }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        {session?.user?.image && (
+          <Image
+            src={session.user.image}
+            width={40}
+            height={40}
+            className="rounded-full"
+            alt="user profile image"
+          />
+        )}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-[250px]">
+        <DropdownMenuLabel>
+          <div className="flex flex-col items-center gap-y-4">
+            {session?.user?.image && (
+              <Image
+                src={session?.user?.image}
+                className="rounded-full"
+                width={80}
+                height={80}
+                alt="profile Image"
+              />
+            )}
+            <p className="font-semibold">{session?.user?.name}</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href={`/profile/${session?.user?.id}`} className="text-sm">
+            Work Preferences
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href={`/profile/${session?.user?.id}`} className="text-sm">
+            Settings
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href={`/profile/${session?.user?.id}`} className="text-sm">
+            Profile
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <button type="button" className="text-sm" onClick={() => signOut()}>
+            Sign out
+          </button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default ProfileMenu;
