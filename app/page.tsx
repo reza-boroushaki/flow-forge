9e7660f84765b150b2b5de5d3bbbbce9a461773e
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2024-03-12 18:42:30 +0330

    update conditional rendering for categories

--- app/page.tsx
+++ app/page.tsx
@@ -12,37 +12,31 @@ export default async function Home({
 }) {
   const data = (await fetchAllProjects(category)) as ProjectInterface[];
 
-  if (data?.length === 0) {
-    return (
-      <section className="flexStart flex-col paddings">
-        <Categories />
-
-        <p className="no-result-text text-center">
-          No projects found, go create some first.
-        </p>
-      </section>
-    );
-  }
-
   return (
     <>
       <HeroParallax products={heroProdcts} />
-      <section className="flexStart flex-col paddings mb-16">
-        <Categories />
 
-        <section className="projects-grid">
-          {data?.map((item) => (
-            <ProjectCard
-              key={item?._id.toString()}
-              title={item?.title}
-              _id={item?._id.toString()}
-              image={item?.image}
-              name={item?.createdBy.name}
-              avatarUrl={item?.createdBy.avatarUrl}
-              userId={item?.createdBy._id.toString()}
-            />
-          ))}
-        </section>
+      <Categories />
+      <section className="flexStart flex-col paddings mb-16">
+        {data?.length === 0 ? (
+          <p className="no-result-text text-center">
+            No projects found, go create some first.
+          </p>
+        ) : (
+          <section className="projects-grid">
+            {data?.map((item) => (
+              <ProjectCard
+                key={item?._id.toString()}
+                title={item?.title}
+                _id={item?._id.toString()}
+                image={item?.image}
+                name={item?.createdBy.name}
+                avatarUrl={item?.createdBy.avatarUrl}
+                userId={item?.createdBy._id.toString()}
+              />
+            ))}
+          </section>
+        )}
       </section>
     </>
   );
